62b9e09b06134112b8b9cba63cb7b9321b260b3f
Yohei TSUJI <yohtsuji@yahoo-corp.jp>
2020-02-07 19:25:16 +0900

    Implement table format for node.

--- fabricchef/tasks/node.py
+++ fabricchef/tasks/node.py
@@ -5,10 +5,13 @@ from __future__ import print_function
 import itertools as itr
 import re
 
-from fabric.colors import green, yellow
+from fabric.colors import green, blue, yellow
 
 from fabricchef.api import *
 from fabricchef.tasks.databag import get_exists_databag_item_names, parse_databag_item_name
+
+import json
+from prettytable import PrettyTable
 
 
 @task
@@ -16,7 +19,35 @@ def list():
     """
     List Nodes in current Environment.
     """
-    knife('search node "chef_environment:%s"' % env.ChefEnv, always_run=True)
+    def print_to_table(result):
+        j = json.loads(result)
+
+        table = PrettyTable(["NodeName", "Platform", "FQDN", "IP Address", "Uptime", "Environment", "RunList"])
+        table.align["NodeName"] = 'l'
+        table.align["Platform"] = 'l'
+        table.align["FQDN"] = 'l'
+        table.align["IP Address"] = 'l'
+        table.align["Uptime"] = 'l'
+        table.align["Environment"] = 'l'
+        table.align["RunList"] = 'l'
+        for i in j:
+            a = i['automatic']
+            table.add_row([
+                i['name'],
+                "%s(%s)" % (a['platform'], a['platform_version']),
+                a['fqdn'],
+                a['ipaddress'],
+                a['uptime'],
+                i['chef_environment'],
+                i['run_list']
+            ])
+        print(table)
+        print("%s Node(s)" % len(j))
+
+    printf(
+        knife('search node "chef_environment:%s"' % env.ChefEnv, always_run=True),
+        print_to_table
+    )
 
 
 @task
@@ -27,10 +58,56 @@ def show(node_name, show_all_attrs='False'):
     :param node_name: Node name.
     :param show_all_attrs: Show all attributes.(True|False) (Default False)
     """
+    def print_all_to_table(result):
+        j = json.loads(result)
+        a = j['automatic']
+
+        print(blue("Node:", bold=True))
+        table1 = PrettyTable()
+        table1.add_column("NodeName", j['name'], 'l')
+        table1.add_column("Platform", "%s(%s)" % (a['platform'], a['platform_version']), 'l')
+        table1.add_column("FQDN", a['fqdn'], 'l')
+        table1.add_column("IP Address", a['ipaddress'], 'l')
+        table1.add_column("Uptime", a['uptime'], 'l')
+        table1.add_column("Environment", j['chef_environment'], 'l')
+        table1.add_column("RunList", j['run_list'], 'l')
+        print(table1)
+
+        print(blue("Device:", bold=True))
+        table2 = PrettyTable()
+        table2.add_column("CPU Cores", a['cpu']['cores'], 'r')
+        table2.add_column("Memory Free/Total", "%s / %s" %
+                          (
+                              a['memory']['free'],
+                              a['memory']['total']
+                          ), 'r')
+        table2.add_column("Disk Free/Total (KB)", "%s / %s" %
+                          (
+                              a['filesystem']['by_mountpoint']['/']['kb_available'],
+                              a['filesystem']['by_mountpoint']['/']['kb_size']
+                          ), 'r')
+        print(table2)
+
+    def print_to_table(result):
+        j = json.loads(result)
+
+        print(blue("Node:", bold=True))
+        table = PrettyTable()
+        table.add_column("NodeName", j['name'], 'l')
+        table.add_column("Environment", j['chef_environment'], 'l')
+        table.add_column("RunList", j['run_list'], 'l')
+        print(table)
+
     if show_all_attrs in ('True', 'true', 'yes'):
-        knife('node show %s -l' % node_name, always_run=True)
+        printf(
+            knife3('node show %s -l' % node_name, always_run=True),
+            print_all_to_table
+        )
     else:
-        knife('node show %s' % node_name, always_run=True)
+        printf(
+            knife3('node show %s' % node_name, always_run=True),
+            print_to_table
+        )
 
 
 @task
@@ -55,9 +132,11 @@ def add(host_name, node_name=None, *accessible_databag_item_patterns):
     _node_name = node_name if node_name and node_name != 'None' else host_name
 
     print(green("Adding Node %s(Host=%s) in Environment %s..." % (_node_name, host_name, env.ChefEnv)))
-    knife(
-        'bootstrap %s --sudo --ssh-user=`whoami` --node-name=%s --environment=%s' %
-        (host_name, _node_name, env.ChefEnv)
+    printt(
+        knife3(
+            'bootstrap %s --sudo --ssh-user=`whoami` --node-name=%s --environment=%s' %
+            (host_name, _node_name, env.ChefEnv)
+        )
     )
 
     if accessible_databag_item_patterns:
@@ -73,7 +152,7 @@ def add(host_name, node_name=None, *accessible_databag_item_patterns):
             for i in databag_item_names_tobe_access:
                 print(green("Grant access to DataBag item %s..." % i))
                 (databag_name, item_name) = parse_databag_item_name(i)
-                knife('vault update %s %s -C "%s"' % (databag_name, item_name, _node_name))
+                printt(knife3('vault update %s %s -C "%s"' % (databag_name, item_name, _node_name)))
         else:
             print(yellow(
                 "No DataBag item matches the specified pattern %s." %
