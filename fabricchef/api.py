9f7ca8348bc311fff005420b73a5a9ce0eab3ca9
Yohei TSUJI <yohtsuji@yahoo-corp.jp>
2020-02-22 13:39:22 +0900

    The create_chef_repo task change to API, and add example task using it.

--- fabricchef/api.py
+++ fabricchef/api.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 import json
 
 from fabric.api import *
-from fabric.colors import red
+from fabric.colors import red, green
 from flatten_json import flatten
 from prettytable import PrettyTable
 
@@ -79,3 +79,48 @@ def printf(fn_knife,
 
 def printt(fn_knife):
     print(fn_knife('text'))
+
+
+def create_chef_repo(chef_server, chef_org, chef_user, client_key_path, editor="vim", repo_dir="."):
+    """
+    Create Chef repository.
+
+    Ex)
+    $ fab create_chef_repo:https://example.com,test-org,scott,~/private_key.pem,emacs
+
+    /example
+      /.chef
+        knife.rb
+        scott.pem
+
+    :param chef_server: Chef server. Like this "https://example.com"
+    :param chef_org: Chef Organization.
+    :param chef_user: Your Chef user name.
+    :param client_key_path: Path for your private key(*.pem) file. Will be copy under chef repository directory.
+    :param editor: Editor you use. (Default vim)
+    :param repo_dir: Target directory. (Default current directory)
+    """
+
+    knife_rb = '''
+current_dir = File.dirname(__FILE__)
+
+log_level                :info
+log_location             STDOUT
+node_name                "%(User)s"
+client_key               "#{current_dir}/%(User)s.pem"
+chef_server_url          "%(ChefServer)s/organizations/%(Organization)s"
+cookbook_path            ["#{current_dir}/../cookbooks"]
+
+knife[:vault_mode] = 'client'
+knife[:editor] = '/usr/bin/%(Editor)s'
+    '''.strip() % dict(
+        ChefServer=chef_server,
+        Organization=chef_org,
+        User=chef_user,
+        Editor=editor
+    )
+
+    print(green("Create Chef repository to %s/.chef..." % repo_dir))
+    local('mkdir -p %s/.chef' % repo_dir)
+    local('cat << EOF > %s/.chef/knife.rb\n%s\nEOF' % (repo_dir, knife_rb))
+    local('cp %s %s/.chef/%s.pem' % (client_key_path, repo_dir, chef_user))
